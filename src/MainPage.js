4de794ec531101cea0c5092479f1f2800cde70b3
Declan Bernardin <dmjbernardin@yahoo.com>
2019-09-29 17:31:47 -0500

    adding images and links to linked in and github page

---
+++ src/MainPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div className="header">
+          <a href="https://www.linkedin.com/in/declan-bernardin-678045191/">LinkedIn</a>
+          <a href="https://github.com/DeclanBernardin">GitHub</a>
+        </div>
+      </header>
+      <div>
+        <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/Untitled-1.png" alt="image of Declan Bernardin" height="150px" width="150px"></img>
+        <h1>Declan Bernardin</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
