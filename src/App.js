99c27835fa0b230b11b19f9947c2a497f7f10812
Declan Bernardin <dmjbernardin@yahoo.com>
2019-09-29 18:22:08 -0500

    adding image to main page and link to projects

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="body">
       <header>
         <div className="header">
           <a href="https://www.linkedin.com/in/declan-bernardin-678045191/">LinkedIn</a>
           <a href="https://github.com/DeclanBernardin">GitHub</a>
         </div>
       </header>
-      <div>
+      <div className="image">
         <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/Untitled-1.png" alt="image of Declan Bernardin" height="150px" width="150px"></img>
         <h1>Declan Bernardin</h1>
+      </div>
+      <div>
+        <h2>Projects:</h2>
+        <h3>(WTF?) Where The Fish?</h3>
+        <img src="" alt="Where The Fish log in page and link to github" src=""></img>
       </div>
     </div>
   );
