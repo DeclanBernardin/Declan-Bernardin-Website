c0a02f17735c84636af68241daa0dad209bf5e18
Declan Bernardin <dmjbernardin@yahoo.com>
2019-10-13 16:49:11 -0500

    added some more stuff

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -12,49 +12,121 @@ const styles = ({
 });
 
 class MainPage extends Component {
+
+    state={
+        about: true,
+        skills: true,
+        projects: true,
+        workExperience: true,
+        education: true,
+    }
+
+toggleAbout = () => {
+    this.setState({
+        about: !this.state.about
+    })
+    console.log(this.state.about);
+    
+}
+
+toggleSkills = () => {
+    this.setState({
+        skills: !this.state.skills
+    })
+    console.log(this.state.skills);
+    
+}
+
+toggleProjects = () => {
+    this.setState({
+        projects: !this.state.projects
+    })
+    console.log(this.state.projects);
+    
+}
+
+toggleWorkExperience = () => {
+    this.setState({
+        workExperience: !this.state.workExperience
+    })
+    console.log(this.state.workExperience);
+    
+}
+
+toggleEducation = () => {
+    this.setState({
+        education: !this.state.education
+    })
+    console.log(this.state.education);
+    
+}
+
     render(){
         return(
             <div className="body">
                 <header >
                     
                     <div className="header">
-                        <a href="#About" ><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px', }} label="About" indicatorColor="primary"
+                        <a href="#About" ><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px', backgroundColor: '#ffe28a' }} label="About" indicatorColor="primary"
                             textColor="primary"/></a>
-                        <a href="#Skills"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px' }}label="Skills" indicatorColor="primary"
+                        <a href="#Skills"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px', backgroundColor: '#ffe28a' }}label="Skills" indicatorColor="primary"
                             textColor="primary"/></a>
-                        <a href="#Projects"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px' }}label="Projects" indicatorColor="primary"
+                        <a href="#Projects"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px', backgroundColor: '#ffe28a' }}label="Projects" indicatorColor="primary"
                             textColor="primary"/></a>
-                        <a href="#WorkExperience"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px' }}label="Work Experience" indicatorColor="primary"
+                        <a href="#WorkExperience"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px', backgroundColor: '#ffe28a' }}label="Work Experience" indicatorColor="primary"
                             textColor="primary"/></a>
-                        <a href="#Education"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px' }}label="Education" indicatorColor="primary"
+                        <a href="#Education"><Tab style={{ color: '#6fcb9f', fontFamily: 'impact', textDecoration: 'none', fontSize: '20px', backgroundColor: '#ffe28a' }}label="Education" indicatorColor="primary"
                             textColor="primary"/></a>
                     </div>
                 </header>
                 <div className="sectionOne">
-                <div className="image">
-                        <img src="https://raw.githubusercontent.com/DeclanBernardin/DeclanBernardin.github.io/master/Untitled-12.png" alt="image of Declan Bernardin" height="300px" width="300px"></img>
-                </div>
+                
+                    <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/Redone-Image.png" alt="image of Declan Bernardin" height="500px"  style={{float: 'Right', margin: '0px', paddingTop: '', }}></img>
                     <div className="contact">
-                    <h1>Declan Bernardin</h1>
+                    <h1>DECLAN BERNARDIN</h1>
                 </div>
                 </div>
-                <section id="About">
-                    <h1 className="About"> About Me </h1>
+                {this.state.about ? <section id="About">
+                    <h1 style={{ textAlign: 'center', color: 'black', }}> About Me </h1>
+                    <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/ClickHere.png" onClick={this.toggleAbout}></img>
                     <a className="linkedIn" href="https://www.linkedin.com/in/declan-bernardin-678045191/"><Tab label={<LinkedInIcon />} /></a>
                     <a className="gitHub" href="https://github.com/DeclanBernardin"><Tab label="GitHub" /></a>
-                </section>
+                </section> :
+                <section id="About">
+                    <h1>About</h1>
+                </section>}
+
+                {this.state.skills ? <section id="Skills">
+                    <h1 style={{ textAlign: 'center', color: 'black', }}>Skills</h1>
+                    <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/ClickHere.png" onClick={this.toggleSkills}></img>
+                </section> :
                 <section id="Skills">
-                    <h1 className="Skills">Skills</h1>
-                </section>
+                    <h1>Skills</h1>
+                </section>}
+
+                {this.state.projects ? <section id="Projects">
+                    <h1 style={{ textAlign: 'center', color: 'black', }}>Projects</h1>
+                    <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/ClickHere.png" onClick={this.toggleProjects}></img>
+                </section> :
                 <section id="Projects">
-                    <h1 className="Projects">Projects</h1>
-                </section>
+                    <h1>Projects</h1>
+                </section>}
+
+                {this.state.workExperience ? <section id="WorkExperience">
+                    <h1 style={{ textAlign: 'center', color: 'black', }}>Work Experience</h1>
+                    <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/ClickHere.png" onClick={this.toggleWorkExperience}></img>  
+                </section> :
                 <section id="WorkExperience">
-                    <h1 className="WorkExperience">Work Experience</h1>
-                </section>
+                    <h1>Work Experience</h1>
+                </section>}
+
+                {this.state.education ? <section id="Education">
+                    <h1 style={{ textAlign: 'center', color: 'black', }}>Education</h1>
+                    <img src="https://raw.githubusercontent.com/DeclanBernardin/Declan-Bernardin-Website/master/ClickHere.png" onClick={this.toggleEducation}></img>
+                </section> :
                 <section id="Education">
-                    <h1 className="Education">Education</h1>
-                </section>
+                    <h1>Education</h1>
+                </section>}
             </div>
         )
     }
